8a9d5fd488a878e39c7203e43bd16ca0629ab60e
Eloi Sanchez <eloisanchez16@gmail.com>
2024-09-21 19:59:25 +0200

    Created and parsed group topics model

---
+++ models/intermediate/parsed_group_topics.sql
@@ -0,0 +1,15 @@
+with
+
+    raw_group_topics as (
+        select data:group_id as group_id, data:topics as topics
+        from {{ ref("raw_groups") }}
+    ),
+
+    parsed_group_topics as (
+        select group_id::varchar as group_id, topics.value::varchar as topic
+        from raw_group_topics, lateral flatten(input => raw_group_topics.topics) topics
+    )
+
+select *
+from parsed_group_topics
+
