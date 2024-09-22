5e133e0a11dbfc99bc33bbab7fddf212df2d8440
Eloi Sanchez <eloisanchez16@gmail.com>
2024-09-22 12:04:15 +0200

    Modified script for dbt_user and added files for superset configuration

--- analyses/superset_user.sql
+++ analyses/superset_user.sql
@@ -1,62 +1,62 @@
 use role securityadmin
 ;
 
-create or replace role dbt_worker_role;
+create or replace role superset_analyst_role;
 
-grant usage, create schema
+grant usage
 on database meetup_assessment
-to role dbt_worker_role
+to role superset_analyst_role
 ;
-grant usage, create table, create view
+grant usage
 on all schemas in database meetup_assessment
-to role dbt_worker_role
+to role superset_analyst_role
 ;
-grant usage, create table, create view
+grant usage
 on future schemas in database meetup_assessment
-to role dbt_worker_role
+to role superset_analyst_role
 ;
 
 grant select
 on all tables in database meetup_assessment
-to role dbt_worker_role
+to role superset_analyst_role
 ;
 grant select
 on future tables in database meetup_assessment
-to role dbt_worker_role
+to role superset_analyst_role
 ;
 grant select
 on all views in database meetup_assessment
-to role dbt_worker_role
+to role superset_analyst_role
 ;
 grant select
 on all views in database meetup_assessment
-to role dbt_worker_role
+to role superset_analyst_role
 ;
 
 
 grant usage
 on warehouse compute_wh
-to role dbt_worker_role
+to role superset_analyst_role
 ;
 
-grant role dbt_worker_role
+grant role superset_analyst_role
 to role sysadmin
 ;
 
 use role useradmin
 ;
 
-create or replace user dbt_worker
+create or replace user superset_analyst
   PASSWORD = 'xyjA28vfQ7gUU0'
   MUST_CHANGE_PASSWORD = FALSE
   DEFAULT_WAREHOUSE = compute_wh
-  DEFAULT_ROLE = dbt_worker_role
-  COMMENT = 'Used by dbt to connect to Snowflake';
+  DEFAULT_ROLE = superset_analyst_role
+  COMMENT = 'Used by superset to connect to Snowflake';
 
 use role securityadmin
 ;
 
-grant role dbt_worker_role
-to user dbt_worker
+grant role superset_analyst_role
+to user superset_analyst
 ;
 
